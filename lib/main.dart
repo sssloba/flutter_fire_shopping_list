7695b75f201aeae15f898640c0f08e2426c2d375
sssloba <slobodan.stojakov@gmail.com>
2022-07-29 10:46:27 +0200

    firebase-setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fire_shopping_list/screens/home_page.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+
+  final test = FirebaseFirestore.instance.collection('orders').id;
+
+  debugPrint(test);
 }
 
 class MyApp extends StatelessWidget {
@@ -15,8 +27,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.deepPurple,
-          snackBarTheme:
-              const SnackBarThemeData(backgroundColor: Colors.deepPurple)),
+          snackBarTheme: const SnackBarThemeData(backgroundColor: Colors.red)),
       home: const HomePage(),
     );
   }
